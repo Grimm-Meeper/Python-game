3f2cffa17c0489d61b4e3d4d887cb52ef573c264
me <fenton@vonmillion.family>
2025-11-18 17:55:40 -0800

    added remove command with the help of chatGPT

--- Main.py
+++ Main.py
@@ -1,28 +1,49 @@
 Places = []
 People = []
+lists = [People, Places]
 def pick_action():
     action = input("What do you want to do?").lower()
     if action == "add":
-        Add_something()
+        Add()
         repet()
+    elif action == "remove":
+        Remove()
     else:
         print("Please try again")
         pick_action()
 
-def Add_something():
+def Add():
     action = input("What to add?").lower()
     if action == "place":
         Places.append(input("Where?"))
     elif action == "person":
         People.append(input("Who?"))
+    elif action == "cansil":
+        print("Okay")
+        repet()
     else:
         print("Try again")
-        Add_something()
+        Add()
 
+def Remove():
+    lists = [People, Places]
+    print(lists)
+    action = input("What to remove?")
+    print(action)
+    for inner in lists:
+        if action in inner:
+            inner.remove(action)
+            repet()
+    if action == "cansil":
+        print("Okay")
+        repet()
+    else:
+        print("Try again")
+        Remove()
 
 def repet():
     print(f"People: {People}, places: {Places}.")
-    if input("Add aonther?(y/n)").lower() == "y":
+    if input("Do something else?(y/n)").lower() == "y":
         pick_action()
     else:
         print(f"People: {People}, places: {Places}.")
